377c00d7ad5bb140a2a73076b3ca686c37630154
euclidespaim <euclidespaim@gmail.com>
2025-05-15 08:50:43 -0300

    Estruturas condicionais

---
+++ ProgramacaoApp-301/Exemplos/ex-2.py
@@ -0,0 +1,10 @@
+# Crie um programa que simule um sistema de login simples. 
+# Peça ao usuário que digite uma senha. Se a senha for igual a "ifc2025", 
+# exiba a mensagem "Acesso permitido". 
+# Caso contrário, exiba "Senha incorreta. Tente novamente."
+
+senha = input("Digite sua senha: ")
+if senha == "elfrida2025":
+    print("Acesso permitido.")
+else:
+    print("Senha incorreta. Tente novamente.")
